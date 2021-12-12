8cfe05eb1c04926c483701c41aa360f946d0cad3
Artur <PowerBearKiller@mail.ru>
2021-12-12 21:40:33 +0300

    HomeWork1FinalFinal

--- HomeWork1.5/Program.cs
+++ HomeWork1.5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Баранович Артур
+//6. * Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+study.pause();
+study.print();
+
+class study
+{
+    static public void print()
+    {
+        Console.WriteLine("Расписание на завтра:\n1.Английский\n2.Геометрия\n3.Алгебра\n4.География");
+    }
+    static public void pause()
+    {
+        Console.ReadKey(true);
+    }
+}
